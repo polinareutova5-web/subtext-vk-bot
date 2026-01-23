fcb926fd6eda79bbbe8e753f32ff3eb2e336698a
polinareutova5-web <polinareutova5@gmail.com>
2026-01-23 17:04:13 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwdweR0BFaH4RbhXJS_huYtDqp1MwCuCnfNP92EsSXYk2zPAu83PDcGQO90XdugdU9qWQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwAwKvY9ySnaUX2gIv2YshpRFMjRDOZ0mkuB0hwtVTB6YDkpVeeSHZFquI4_1pe8j92uQ/exec";
 
 let userId;
 let userData;
